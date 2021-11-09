6b3b1fde04ae9639a8a1bc2d22d9eb6231b8e14d
Victor Jonah <victorjonah199@gmail.com>
2021-11-09 16:16:42 +0100

    ...

--- test/Token.js
+++ test/Token.js
@@ -1,20 +1,29 @@
 const { expect } = require('chai');
-const { ethers } = require('ethers');
 
 describe('Token contract', () => {
-  let Token, token, addr1, addr2;
+  let Token, token, owner, addr1, addr2;
 
   beforeEach(async () => {
     Token = await ethers.getContractFactory('Token');
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
     token = await Token.deploy();
-    [owner, addr1, addr2, _] = await ethers.getSigners();
   });
 
   describe('Deployment', () => {
     it('should set the right owner', async () => {
-      expect(await token.owner().to.equal(owner.address));
+      expect(await token.owner()).to.equal(owner.address);
     });
   });
 
-  // describe('T')
+  describe('Transactions', () => {
+    it('Should transfer tokens between accounts', async () => {
+      await token.transfer(addr1.address, 50);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(50);
+
+      await token.connect(addr1).transfer(addr2.address, 50);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+  });
 });
